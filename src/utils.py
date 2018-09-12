095743554beff6b3b1502da22151d51e9c8936eb
root <root@localhost.localdomain>
2018-09-12 19:08:34 +0800

    add text clean and segment

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8 -*-
 
+import re
+import jieba
+import jieba.posseg
 
 class IterDocument(object):
     """
@@ -26,3 +29,66 @@ class IterDocument(object):
                 yield [item for item in line.split(self.sep) if item != ' ' and item != '']
             else:
                 yield line
+
+
+class TextCleaner:
+    """
+    A class for cleaning text
+    """
+    def __init__(self, punctuation=True, number=True, normalize=True):
+        """
+        :param punctuation: whether clean punctuation
+        :param number: whether clean number
+        :param normalize: whether normalize token
+        """
+        self.punc = punctuation
+        self.num = number
+        self.norm = normalize
+
+        self.punctuation = IterDocument("resource/punctuation")
+    def clean(self, text):
+        """
+        :param text: the raw string
+        :return: the string after cleaning
+        """
+        if self.punc:
+            for p in self.punctuation:
+                text = re.sub(p, "", text)
+
+        return text.strip()
+
+
+class Segmentor:
+    """
+    A class for segmenting text
+    """
+    def __init__(self, user_dict=True):
+        """
+        :param user_dict: whether use user dict
+        """
+        self.seg = jieba
+        self.seg_pos = jieba.posseg
+        if user_dict:
+            self.seg.load_userdict("resource/userdict")
+
+    def seg_token(self, text):
+        """
+        :param text: the raw string
+        :return: a list of token
+        """
+        return self.seg.lcut(text)
+
+    def seg_token_pos(self, text):
+        """
+        :param text: the raw string
+        :return: a list of token/pos
+        """
+        return ["%s/%s" % (token, pos) for token, pos in self.seg_pos.lcut(text)]
+if __name__ == "__main__":
+    tc = TextCleaner()
+    seg = Segmentor()
+    text = "东风夜放花千树，更吹落，星如雨？！"
+    text = tc.clean(text)
+    print text
+    print " ".join(seg.seg_token(text))
+    print " ".join(seg.seg_token_pos(text))
